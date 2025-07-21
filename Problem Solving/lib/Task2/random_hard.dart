c2e3c55b3319fe9792f2fb3fc823bbfce6fcde6e
Mohamed Hussein <mohussein1279@gmail.com>
2025-07-21 20:10:03 +0300

    modify hard game

--- Problem Solving/lib/Task2/random_hard.dart
+++ Problem Solving/lib/Task2/random_hard.dart
@@ -6,16 +6,29 @@ main() {
   int min = int.parse(stdin.readLineSync()!);
   int max = int.parse(stdin.readLineSync()!);
 
-
   int correctCounter = 0;
   int levelCounter = 1;
   print("Welcome to level $levelCounter of Random Game :)");
+  List operators = ['+', '*', '-', '/'];
+  var op = operators[Random().nextInt(operators.length)];
+  op = '+';
   while (true) {
-    int number1 = Random().nextInt(max - min + 1);
-    int number2 = Random().nextInt(max - min + 1);
-    print("$number1 + $number2 = ");
+    int number1 = Random().nextInt(max - min) + 1;
+    int number2 = Random().nextInt(max - min) + 11;
+
+    print("$number1 $op $number2 = ");
+    double ans = 0;
+    if (op == '+') {
+      ans = (number1 + number2).toDouble();
+    } else if (op == '-') {
+      ans = (number1 - number2).toDouble();
+    } else if (op == '*') {
+      ans = (number1 * number2).toDouble();
+    } else if (op == '/') {
+      ans = (number1 / number2).toDouble();
+    }
     int result = int.parse(stdin.readLineSync()!);
-    if (result == number1 + number2) {
+    if (result == ans) {
       print("Correct!");
       correctCounter++;
     } else if (result != number1 + number2) {
@@ -25,6 +38,8 @@ main() {
     if (correctCounter == 3) {
       print("Congratulations you have qualified for the next level");
       levelCounter++;
+      op = operators[Random().nextInt(operators.length)];
+      correctCounter = 0;
       print("Welcome to level $levelCounter of Random Game :)");
     }
     print("Do you want to continue");
